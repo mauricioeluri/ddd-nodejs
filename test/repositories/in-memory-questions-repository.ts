8b6523e849bab8ea941c4e7943cb417673dbc782
Maurício El Uri <mauriciom.eluri@gmail.com>
2025-04-30 03:22:59 -0300

    feat: delete question also deleting attachments

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,10 +1,15 @@
 import { PaginationParams } from "@/core/repositories/pagination-params"
+import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository"
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository"
 import { Question } from "@/domain/forum/enterprise/entities/question"
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
   
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository
+  ) {}
+
   async findById(id: string) {
     const question = this.items.find((item) => item.id.toString() === id)
     if (!question) {
@@ -41,5 +46,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   async delete(question: Question): Promise<void> {
     const intemIndex = this.items.findIndex((item) => item.id === question.id)
     this.items.splice(intemIndex, 1)
+    this.questionAttachmentsRepository.deleteManyByQuestionId(
+      question.id.toString(),
+    )
   }
 }
