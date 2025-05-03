eb141570ce0cfa8cf59f7eb33f8960e92a68b38c
Maurício El Uri <mauriciom.eluri@gmail.com>
2025-05-03 12:51:22 -0300

    feat: subscribers event

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from "@/core/events/domain-events"
 import { PaginationParams } from "@/core/repositories/pagination-params"
 import { AnswerAttachmentsRepository } from "@/domain/forum/application/repositories/answer-attachments-repository"
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository"
@@ -28,11 +29,15 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer) {
     this.items.push(answer)
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async save(answer: Answer) {
     const intemIndex = this.items.findIndex((item) => item.id === answer.id)
     this.items[intemIndex] = answer
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async delete(answer: Answer): Promise<void> {
